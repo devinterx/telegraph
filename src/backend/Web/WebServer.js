7843c5c5e6a6df864ff3e9ea059b66e7dc7202dc
iqueak <iqueak@gmail.com>
2018-01-05 19:45:48 +0100

    + bodyParser, + users CREATE (CRUD)

--- src/backend/Web/WebServer.js
+++ src/backend/Web/WebServer.js
@@ -1,13 +1,17 @@
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 import path from "path";
 
 import Database from "../Database/Database"
+import User from "../User/User"
 import {session} from "../Auth/Auth"
 
 export default class WebServer {
     static init(server, express) {
         server.use(express.static(path.resolve('./public')));
         server.use(cookieParser());
+        server.use(bodyParser.json());
         server.use(session);
 
         server.get('/api/users', (request, response) => {
@@ -15,6 +19,27 @@ export default class WebServer {
                 if (permission > 0) {
                     Database.list('users', {}, results => {
                         response.send(JSON.stringify(results));
+                    });
+                } else response.status(403).json({error: 'Not enough permissions'});
+            });
+        });
+
+        server.post('/api/users', (request, response) => {
+            Database.load('sessions', {token: request.cookies['token']}, ({permission}) => {
+                if (permission > 0) {
+                    let user = request.body['user'];
+                    if (typeof user.id === 'number') user.id = user.id.toString();
+                    Database.find('users', {id: user.id}, results => {
+                        if (results === null) {
+                            if (!(user instanceof User)) user = new User(user);
+                            user._lastUpdateTime = Date.now();
+                            Database.save('users', {id: user.id}, Object.assign({}, user), () => {
+                                User._users[user.id] = user;
+                            });
+                            response.send("Success! User created!");
+                        } else {
+                            response.status(400).json({error: 'User with this id exist!'});
+                        }
                     });
                 } else response.status(403).json({error: 'Not enough permissions'});
             });

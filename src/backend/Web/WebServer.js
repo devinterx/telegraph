46b069c3ace0b70bdf64b94995210419b2c0bf1f
iqueak <iqueak@gmail.com>
2018-01-09 21:49:11 +0100

    + GET:id (READ), GET/?limit=<?:50>&offset=<?:0> (READ), PUT (UPDATE), DELETE (DELETE) https://github.com/devinterx/telegraph/issues/6

--- src/backend/Web/WebServer.js
+++ src/backend/Web/WebServer.js
@@ -14,12 +14,34 @@ export default class WebServer {
         server.use(bodyParser.json());
         server.use(session);
 
-        server.get('/api/users', (request, response) => {
+        server.get('/api/users/:id', (request, response) => {
             Database.load('sessions', {token: request.cookies['token']}, ({permission}) => {
                 if (permission > 0) {
-                    Database.list('users', {}, results => {
-                        response.send(JSON.stringify(results));
+                    let userId = request.params.id;
+                    if (typeof userId === 'number') userId = userId.toString();
+                    Database.find('users', {id: userId}, results => {
+                        if (results !== null) {
+                            response.send(JSON.stringify(results));
+                        } else {
+                            response.status(409).json({error: 'User with this id not exist'});
+                        }
                     });
+                } else response.status(403).json({error: 'Not enough permissions'});
+            });
+        });
+
+        server.get('/api/users/', (request, response) => {
+            Database.load('sessions', {token: request.cookies['token']}, ({permission}) => {
+                if (permission > 0) {
+                    let limit = !isNaN(parseInt(request.query.limit)) ? parseInt(request.query.limit) : 50;
+                    let offset = !isNaN(parseInt(request.query.offset)) ? parseInt(request.query.offset) : 0;
+                    Database.list('users', {}, results => {
+                        if (results !== null) {
+                            response.send(JSON.stringify(results));
+                        } else {
+                            response.status(409).json({error: 'Users not exist'});
+                        }
+                    },limit,offset);
                 } else response.status(403).json({error: 'Not enough permissions'});
             });
         });
@@ -39,6 +61,47 @@ export default class WebServer {
                             response.status(200).json({error: false, message: 'User created'});
                         } else {
                             response.status(409).json({error: 'User with this id exist'});
+                        }
+                    });
+                } else response.status(403).json({error: 'Not enough permissions'});
+            });
+        });
+
+        server.put('/api/users/:id', (request, response) => {
+            Database.load('sessions', {token: request.cookies['token']}, ({permission}) => {
+                if (permission > 0) {
+                    let user = request.body['user'];
+                    user.id = request.params.id;
+                    if (typeof user.id === 'number') user.id = user.id.toString();
+                    Database.find('users', {id: user.id}, results => {
+                        if (results !== null) {
+                            user = new User(user);
+                            user._lastUpdateTime = Date.now();
+                            Database.save('users', {id: user.id}, Object.assign({}, user), () => {
+                                User._users[user.id] = user;
+                            });
+                            response.status(200).json({error: false, message: 'User updated'});
+                        } else {
+                            response.status(409).json({error: 'User with this id not exist'});
+                        }
+                    });
+                } else response.status(403).json({error: 'Not enough permissions'});
+            });
+        });
+
+        server.delete('/api/users/:id', (request, response) => {
+            Database.load('sessions', {token: request.cookies['token']}, ({permission}) => {
+                if (permission > 0) {
+                    let userId = request.params.id;
+                    if (typeof userId === 'number') userId = userId.toString();
+                    Database.find('users', {id: userId}, results => {
+                        if (results !== null) {
+                            Database.remove('users', {id: userId}, () => {
+                                delete User._users[userId];
+                            });
+                            response.status(200).json({error: false, message: 'User deleted'});
+                        } else {
+                            response.status(409).json({error: 'User with this id not exist'});
                         }
                     });
                 } else response.status(403).json({error: 'Not enough permissions'});
